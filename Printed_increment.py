041c0bc40a8a2a7f4b10f18eb66b2bae3f981331
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-08-21 20:18:11 +0530

    Printed_increment.py
    0
    0 1
    0 1 2
    0 1 2 3
    
    1
    1 2
    1 2 3

---
+++ Printed_increment.py
@@ -0,0 +1,12 @@
+n = 5
+for i in range (n):
+    for j in range(i):
+        print(j , end = " ")
+    print()
+
+n = 5
+for i in range (1 ,n):
+    for j in range(1, i):
+        print(j, end = " ")
+    print()
+  
